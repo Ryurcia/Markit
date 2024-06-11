0c5162b54b49e5e90c067cb1e6007190075fc446
RylePC <urciaryle@gmail.com>
2024-06-11 13:59:12 -0400

    fix:Chnaged the select component in Create post to a customized dropdown menu

--- src/app/(protected)/post/createPost/page.tsx
+++ src/app/(protected)/post/createPost/page.tsx
@@ -1,8 +1,14 @@
 'use client';
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { useState } from 'react';
 import dynamic from 'next/dynamic';
 import { poppins } from '@/utils/font.config';
+import { IoChevronDown } from 'react-icons/io5';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 
 const SaleForm = dynamic(() => import('@/components/forms/sale-form'), {
   loading: () => <h1>LOADING</h1>,
@@ -19,35 +25,45 @@ enum FormType {
 }
 
 const page = () => {
-  const [formType, setFormType] = useState<string>('');
+  const [formType, setFormType] = useState<string>('Select type');
 
   return (
     <div
-      className={`w-[90%] md:w-[95%] md:px-0 mx-auto mt-[32px] pb-[20px] grid grid-row-2 grid-cols-1 items-center justify-center gap-6 md:justify-center `}
+      className={`${poppins.className} w-[90%] md:w-[95%] md:px-0 mx-auto mt-[32px] pb-[20px] grid grid-row-2 grid-cols-1 items-center justify-center gap-6 md:justify-center `}
     >
-      <h1 className={`${poppins.className} text-h3 font-semibold`}>Create Post</h1>
+      <h1 className={`text-h3 font-semibold`}>Create Post</h1>
 
       <div className={`grid items-center`}>
         <h1 className={`text-[20px] font-semibold mb-[10px]`}>Select Category</h1>
-        <Select onValueChange={(e) => setFormType(e)}>
-          <SelectTrigger className='w-[180px] text-lg rounded border-0 bg-dark px-[15px] py-[20px] '>
-            <SelectValue placeholder={'Select type'} />
-          </SelectTrigger>
-          <SelectContent className={`rounded bg-dark border-0 `}>
-            <SelectItem value={FormType.SALE} className={`hover:bg-primary hover:rounded text-lg`}>
+        <DropdownMenu modal={false}>
+          <DropdownMenuTrigger
+            className={`w-[180px] border-[1px] border-neutral py-[10px] rounded bg-dark flex justify-between items-center px-[10px]`}
+          >
+            {formType} <IoChevronDown fontSize={16} />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className={`w-[180px] border-0 bg-dark rounded`}>
+            <DropdownMenuItem
+              className={`text-base p-3 hover:bg-primary rounded`}
+              onClick={() => setFormType(FormType.SALE)}
+            >
               Sale
-            </SelectItem>
-            <SelectItem value={FormType.JOB} className={`hover:bg-primary hover:rounded text-lg`}>
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              className={`text-base p-3 hover:bg-primary rounded`}
+              onClick={() => setFormType(FormType.JOB)}
+            >
               Job
-            </SelectItem>
-            <SelectItem value={FormType.SERVICE} className={`hover:bg-primary hover:rounded text-lg`}>
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              className={`text-base p-3 hover:bg-primary rounded`}
+              onClick={() => setFormType(FormType.SERVICE)}
+            >
               Service
-            </SelectItem>
-          </SelectContent>
-        </Select>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
 
-      {/* Lazy render different forms based on form type */}
       <div className={`w-full`}>
         {formType === FormType.SALE && <SaleForm />}
         {formType === FormType.JOB && <JobForm />}
